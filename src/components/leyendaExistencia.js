78ed5ca4949cf0bca10eec7bb0cde27ce9d2d815
CastellanosC <ccastellanos@besign.com.ve>
2021-05-21 10:43:20 -0400

    Actualizaciones y correcciones LTP v4: Correciones en vista de detalle y icons

--- src/components/leyendaExistencia.js
+++ src/components/leyendaExistencia.js
@@ -3,18 +3,21 @@ import React from 'react'
 const LeyendaExitencia = () => {
     return(
         <div className="leyenda-existencia">
-            <div className="row">
-                
-                <div className="col-lg-4 col-md-12 indicador-group">
-                    <div className={`availability small high`}></div><span className="">Disponibilidad alta</span>
-                </div>
-                <div className="col-lg-4 col-md-12 indicador-group">
-                    <div className={`availability small medium`}></div><span className="">Disponibilidad baja</span>
-                </div>
-                <div className="col-lg-4 col-md-12 indicador-group">
-                    <div className={`availability small low`}></div><span className="">No disponible</span>
+            <div className=" container">
+                <div className="row">
+                    
+                    <div className="col-lg-4 col-md-12 indicador-group">
+                        <div className={`availability small high`}></div><span className="">Disponible</span>
+                    </div>
+                    <div className="col-lg-4 col-md-12 indicador-group">
+                        <div className={`availability small medium`}></div><span className="">Pocas Unidades</span>
+                    </div>
+                    <div className="col-lg-4 col-md-12 indicador-group">
+                        <div className={`availability small low`}></div><span className="">No disponible</span>
+                    </div>
                 </div>
             </div>
+            
                 
         </div>
             
