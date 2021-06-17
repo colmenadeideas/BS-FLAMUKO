e1168aa03033f79fa9f3ccd4ad1b6b273d3eedf5
CastellanosC <ccastellanos@besign.com.ve>
2021-06-17 18:16:31 -0400

    Build

---
+++ build/precache-manifest.d6ae79e5b0c9abf5f36375bd70f61b73.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fe087cd29a3a1cb5c513c1fc031e159",
+    "url": "/latiendadelpintor/index.html"
+  },
+  {
+    "revision": "8f0e038db3c43c07b20a",
+    "url": "/latiendadelpintor/static/css/2.9ee52aab.chunk.css"
+  },
+  {
+    "revision": "4572c45c2beae6f4935e",
+    "url": "/latiendadelpintor/static/css/main.0f4099f0.chunk.css"
+  },
+  {
+    "revision": "8f0e038db3c43c07b20a",
+    "url": "/latiendadelpintor/static/js/2.70049c71.chunk.js"
+  },
+  {
+    "revision": "f87b5fca5cc02c7ed333bb6ac439eb38",
+    "url": "/latiendadelpintor/static/js/2.70049c71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4572c45c2beae6f4935e",
+    "url": "/latiendadelpintor/static/js/main.e9d0a644.chunk.js"
+  },
+  {
+    "revision": "a3d2fd0406758dc7b47a",
+    "url": "/latiendadelpintor/static/js/runtime-main.2ddd6451.js"
+  },
+  {
+    "revision": "a23ab6cd37d98fe500db22699e36c5f6",
+    "url": "/latiendadelpintor/static/media/home-buscador-background.a23ab6cd.jpg"
+  }
+]);
